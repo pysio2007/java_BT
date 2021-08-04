d18919ca06ed11a0c439c4ec01b1d2afc7766dc8
Pysio <qq593277393@outlook.com>
2021-08-04 12:09:35 +0800

    变量

---
+++ src/Test01.java
@@ -0,0 +1,15 @@
+//变量
+public class Test01 {
+    public static void main(String[] args) {
+        int age = 18;
+        int _age = 19;
+        int $age = 10;
+        int age123 = 11;
+        //int  123age = 22;  数字不能做开头
+        //int age# =23; 标识符只能是 字母 数字 下划线 $
+        // int class = 24; Class是关键字，不能用做变量
+        int class_ = 24; //加一个下划线就可以了
+        int 年龄 =24; //Java使用的是Unicode字符集，可以使用中文。但是不建议使用！
+
+    }
+}
